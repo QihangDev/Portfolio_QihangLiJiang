c17f98e7ad1912605c2b6f7675dbc32c08de0ef2
Qihang <qihangdev@gmail.com>
2025-06-24 14:18:34 +0200

    created angular project

---
+++ portfolio-qihang/src/app/component/navigation-bar/navigation-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.html',
+  styleUrl: './navigation-bar.css'
+})
+export class NavigationBar {
+
+}
